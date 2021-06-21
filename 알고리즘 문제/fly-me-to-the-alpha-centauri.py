bf2c9e2007080b81b39804977600d61bf38e352c
unknown <hh4518@gmail.com>
2021-06-21 18:04:07 +0900

    edit alpha centauri code

--- 알고리즘 문제/fly-me-to-the-alpha-centauri.py
+++ 알고리즘 문제/fly-me-to-the-alpha-centauri.py
@@ -1,15 +1,15 @@
 from sys import stdin
 
-input = stdin.readline
-
-test_case = int(input())
-
 def number_of_operations(distance):
     square_root = distance ** 0.5
 
     if round(square_root) >= square_root:
-        return round(distance ** 0.5) * 2 - 1
-    return round(distance ** 0.5) * 2
+        return round(square_root) * 2 - 1
+    return round(square_root) * 2
+
+input = stdin.readline
+
+test_case = int(input())
 
 for _ in range(test_case):
     x, y = map(int, input().split())
